603775b013cfeb983bcb916681156eeb45ec4f1c
Marcell Magyar <magyar321@msn.com>
2018-05-08 13:03:15 +0700

    Added functional timer with start/stop/pause buttons

--- script/script.js
+++ script/script.js
@@ -70,27 +70,51 @@ function generate(arr) {
 }
 
 //timer function from stackoverflow
-function timer(time,update,complete) {
-    var start = new Date().getTime();
-    var interval = setInterval(function() {
-        var now = time-(new Date().getTime()-start);
-        if( now <= 0) {
-            clearInterval(interval);
-            complete();
-        }
-        else update(Math.floor(now/1000));
-    },100); // the smaller this number, the more accurate the timer will be
-}
+    var CCOUNT = 60;
+    
+    var t;
+    var count = 60;
 
-$("#start").on('click', function(){
-	// milliseconds 
-	timer(60000, function(timeleft) {
-	// called every step to update the visible countdown
-    $(".timer").text(timeleft)		
-    },
-    function() { // what to do after
-    $(".timer").text("Time's up!!!");
-    }
-);
-});
+    
+    function cddisplay() {
+        // displays time in span
+        $(".timer").text(count);
+    };
+    
+    function countdown() {
+        // starts countdown
+        cddisplay();
+        if (count == 0) {
+            // time is up
+        } else {
+            count--;
+            t = setTimeout("countdown()", 1000);
+        }
+    };
+    
+    function cdpause() {
+        // pauses countdown
+        clearTimeout(t);
+    };
+    
+    function cdreset() {
+        // resets countdown
+        cdpause();
+        count = CCOUNT;
+        cddisplay();
+    };
+
+    $("#start").click(function() {
+    	countdown();
+    });
+
+    $("#stop").click(function() {
+    	cdpause();
+    })
+
+    $("#reset").click(function() {
+    	cdreset();
+    })
+
+
 
